35294c2933177abdccf5965ab837b37648a0bd67
Harry <harry@hjb.dev>
2021-09-08 21:45:57 +0100

    Engine Tests, made engine method names better

--- src/Engine.php
+++ src/Engine.php
@@ -6,23 +6,25 @@ use Illuminate\Http\Request;
 
 class Engine
 {
-    private HasFields $model;
+    private object $model;
 
-    public function __construct(HasFields $model)
+    public function __construct(object $model)
     {
         $this->model = $model;
     }
 
-    public static function processRequest(HasFields $model, Request $request) 
+    public static function request(object $model, Request|array $request) 
     {
         $instance = new static($model);
-        return $instance->visible($request);
+        return $instance->processRequest($request);
     }
 
-    public function visible(Request $request) 
+    public function processRequest(Request|array $request) 
     {
         // Get the request data
-        $request = $request->all();
+        if ($request instanceof Request) {
+            $request = $request->all();
+        }
 
         // Retrieve field definitions
         $fields = $this->model->keyedFields();
@@ -36,48 +38,59 @@ class Engine
 
             $conditions = $field->visible;
 
-            if (is_bool($conditions)) {
-                // If it's a hardcoded boolean, return it.
-                return $conditions;
-            } else if (is_callable($conditions)) {
-                // If it's a closure, return it, given the full request.
-                return $conditions($request);
-            } else if (is_array($conditions)) {
-                // This should be an array of conditions, we need to do some checking here.
-                $vis = false;
-                // loop through the conditions
-                foreach ($conditions as $condition) {
-                    if (count($condition) === 2) {
-                        // two conditions
-                        if ($request[$condition[0]] === $condition[1]) {
-                            $vis = true;
-                        } else {
-                            return false;
-                        }
-                    } else if (count($condition) === 3) {
-                        // three elements, middle is operator
-                        $result = false;
-                        switch ($condition[1]) {
-                            default:
-                            case '=':
-                            case '==':  $result = $condition[0] == $condition[2]; break;
-                            case '!=':
-                            case '<>':  $result = $condition[0] != $condition[2]; break;
-                            case '<':   $result = $condition[0] < $condition[2]; break;
-                            case '>':   $result = $condition[0] > $condition[2]; break;
-                            case '<=':  $result = $condition[0] <= $condition[2]; break;
-                            case '>=':  $result = $condition[0] >= $condition[2]; break;
-                            case '===': $result = $condition[0] === $condition[2]; break;
-                            case '!==': $result = $condition[0] !== $condition[2]; break;
-                        }
-                        // If it's true, continue looping.
-                        if(!$result) return false;
+            $isVisible = $this->checkConditions($conditions, $request);
+
+            if (!$isVisible) {
+                unset($fields[$key]);
+            }
+        }
+
+        return $fields;
+    }
+
+    protected function checkConditions($conditions, $request)
+    {
+        if (is_bool($conditions)) {
+            // If it's a hardcoded boolean, return it.
+            return $conditions;
+        } else if (is_callable($conditions)) {
+            // If it's a closure, return it, given the full request.
+            return $conditions($request);
+        } else if (is_array($conditions)) {
+            // This should be an array of conditions, we need to do some checking here.
+            $vis = false;
+            // loop through the conditions
+            foreach ($conditions as $condition) {
+                if (count($condition) === 2) {
+                    // two conditions
+                    if ($request[$condition[0]] === $condition[1]) {
+                        $vis = true;
+                    } else {
+                        return false;
                     }
+                } else if (count($condition) === 3) {
+                    // three elements, middle is operator
+                    $result = false;
+                    switch ($condition[1]) {
+                        default:
+                        case '=':
+                        case '==':  $result = $condition[0] == $condition[2]; break;
+                        case '!=':
+                        case '<>':  $result = $condition[0] != $condition[2]; break;
+                        case '<':   $result = $condition[0] < $condition[2]; break;
+                        case '>':   $result = $condition[0] > $condition[2]; break;
+                        case '<=':  $result = $condition[0] <= $condition[2]; break;
+                        case '>=':  $result = $condition[0] >= $condition[2]; break;
+                        case '===': $result = $condition[0] === $condition[2]; break;
+                        case '!==': $result = $condition[0] !== $condition[2]; break;
+                    }
+                    // If it's true, continue looping.
+                    if(!$result) return false;
                 }
             }
-
-            return false;
         }
+
+        return false;
     }
 
 }
--- tests/EngineTest.php
+++ tests/EngineTest.php
@@ -1,2 +1,27 @@
 <?php
 
+use Engine\Engine;
+use Engine\Fields\Text;
+
+class FakeModel {
+    use \Engine\HasFields;
+    public function fields() {
+        return [
+            Text::create('First Name'),
+            Text::create('Last Name')->visible('first_name', '!=', null)
+        ];
+    }
+}
+
+test('engine validates a request successfully', function () {
+    $model =  new FakeModel();
+
+    $request = [
+        'first_name' => null,
+        'last_name' => null
+    ];
+
+    $fields = Engine::request($model, $request);
+
+    expect($fields)->toHaveCount(1);
+});
